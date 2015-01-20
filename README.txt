9f8b11953872a72b5dcc4213cdeb4434826aff97
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 16:34:54 +0000

    New version published.
    git-svn-id: https://plugins.svn.wordpress.org/cp-reservation-calendar/tags/1.1.6@1071799 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,155 @@
+=== CP Reservation Calendar ===
+Contributors: codepeople
+Donate link: http://wordpress.dwbooster.com/calendars/booking-calendar-contact-form
+Tags: reservation calendar,booking calendar,bookings,reservations,paypal,calendar,reservation,booking,hotel,room
+Requires at least: 3.0.5
+Tested up to: 4.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+CP Reservation Calendar is a booking calendar that allows selecting dates - ex: check-in and check-out dates - for a reservation. 
+
+== Description ==
+
+CP Reservation Calendar is a booking calendar that allows selecting dates - ex: check-in and check-out dates - for a reservation. 
+
+= Main Features: =
+
+1. The reservation calendar supports **partial days** and **complete days** reservations
+2. Booking form integrated with payment options
+3. Email notifications 
+4. Easy setup
+5. Multiple configuration options
+6. One-click publishing process 
+
+This **booking calendar** can be used for hotel bookings, tour bookings, car rentals, office rentals, jewels rentals, cellphone rentals, etc. It's **integrated with PayPal** for receiving the reservation payments and has many other features (details below).
+
+The reservation calendar can be configured in two modes: "complete day" or "partial day" bookings. *Complete day* means that the first and the last days booked are charged as full days while *Partial Day* means that they are charged as half-days only. Partial Day system is widely used by hotels and car rental systems: the customer is charged half a daily rate for both the arrival/pick-up day and the departure/drop-off day, thereby matching the number of nights actually spent in the hotel or the 24 hour days using the service.
+
+= More Features: =
+
+* The website visitors can **select a start date and end date for the reservation** and pay for it through **PayPal / credit cards**.
+* The amount to pay is **calculated** based in the number of days of the reservation.
+* Once a reservation has been booked it appears as blocked in order to **prevent duplicated bookings**.
+* You receive an **email** after each confirmed (paid) booking.
+* The user receives a **thank you/confirmation email** after completing the payment.
+* Allows **customizing the contents** of the notification/confirmation emails.
+* Allows defining the product name at PayPal, the currency, the PayPal language and amount to pay for a booking (you can set zero to let the user pay/donate the desired amount).
+* You can define the **start day** of the week.
+* You can define the **minimum available date** and the **maximum available date** for the booking calendar.
+* You can **block specific dates**.
+* You can **assign a user** to the calendar. The owner (with Editor Access Level) can edit only their own calendar.
+* You can **publish the calendars of a specific user** with the shorttag *[CP_RESERVATION_CALENDAR user="admin"]*
+* You can **publish a specific calendar** with the shorttag *[CP_RESERVATION_CALENDAR calendar="1"]*
+* Can be configured in two modes: **"complete day"** or **"partial day"** bookings.
+* Easy Ajax administration for the calendar.
+
+== Installation ==
+
+To install **CP Reservation Calendar**, follow these steps:
+
+1.	Download and unzip the CP Reservation Calendar plugin
+2.	Upload the entire cp-reservation-calendar/ directory to the /wp-content/plugins/ directory
+3.	Activate the  CP Reservation Calendar plugin through the Plugins menu in WordPress
+4.	Configure the booking calendar settings at the administration menu >> Settings >> CP Reservation Calendar.
+5.	To insert the reservation calendar form into some content or post use the icon that will appear when editing contents
+
+To update the plugin you can either overwrite the files or upload it via Plugins - Add New - Upload.
+
+== Frequently Asked Questions ==
+
+= Q: What means each field in the booking calendar settings area? =
+
+A: The product's page contains detailed information about each field and customization:
+
+http://wordpress.dwbooster.com/calendars/cp-reservation-calendar
+
+= Q: How can I change the calendar's size and colors? =
+
+A: Go to the file "cp-reservation-calendar/TDE_RCalendar/all-css.css" and edit the following styles:
+
+Line #13 - style for the external border; background and border:
+
+    .yui-calcontainer { 
+      float:left;    padding:5px; background-color:#F7F9FB; border:1px solid #7B9EBD;
+    }
+
+
+Line #42 - style for width, height, border and background for all cells:
+
+    .yui-calendar td.calcell {
+      width:2em; height:2em; border:1px solid #E0E0E0; background-color:#FFF;
+    }
+
+
+Line #46 - style for the current month's cells:
+
+    .yui-calendar td.calcell a {
+      color:#003DB8; text-decoration:none; 
+    }
+
+
+Line #53 - style for the cells with days that do not belong to the current month:
+
+    .yui-calendar td.calcell.oom {   
+      cursor:default; color:#999; background-color:#EEE; border:1px solid #E0E0E0; 
+    }
+
+
+Line #81 - style cells with associated information (events, links):
+
+    .yui-calendar td.calcell.reserved {
+      background-color:#FFff00;
+    } 
+
+
+Line #109 - style for day's names:
+
+    .yui-calendar .calweekdaycell {
+      color:#666; font-weight:normal;
+    }
+    
+= Q: I'm getting this message: "Destination folder already exists". Solution? =
+
+A: The currently installed version of the plugin (if any) must be deleted before installing a new version.
+
+This is a safe step, the plugin's data and settings won't be lost during the process.
+
+Another alternative is to overwrite the plugin files through a FTP connection. This is also a safe step.
+    
+== Screenshots ==
+
+1. Inserting the reservation calendar into a page.
+2. Booking form/reservation form.
+3. Booking calendar configuration and administration.
+
+== Changelog ==
+
+= 1.0 =
+* First stable version released.
+
+= 1.0.1 =
+* Little bugs fixed
+* More configuration settings added
+* Compatible with WP 3.8
+* New configuration settings and improvements.
+
+= 1.1.6 =
+* Compatible with the latest WP versions
+* Better interface and access to the plugin options
+* Minor bug fixes
+
+Important note: If you are using the Professional version don't update via the WP dashboard but using your personal update link. Contact us if you need further information: http://wordpress.dwbooster.com/support
+
+== Upgrade Notice ==
+
+Very Important note: If you are using the Professional version don't update via the WP dashboard but using your personal update link. Contact us if you need further information: http://wordpress.dwbooster.com/support
+
+= 1.1.6 =
+* Compatible with the latest WP versions
+* Better interface and access to the plugin options
+* Minor bug fixes
+
+
+Important note: If you are using the Professional version don't update via the WP dashboard but using your personal update link. Contact us if you need further information: http://wordpress.dwbooster.com/support
---
+++ dex_reservations_admin_int_calendar_list.inc.php
@@ -0,0 +1,189 @@
+<?php
+
+if ( !is_admin() ) 
+{
+    echo 'Direct access not allowed.';
+    exit;
+}
+
+$current_user = wp_get_current_user();
+
+global $wpdb;
+$message = "";
+if (isset($_GET['a']) && $_GET['a'] == '1')
+{
+    $sql .= 'INSERT INTO `'.DEX_RESERVATIONS_CONFIG_TABLE_NAME.'` (`'.TDE_RESERVATIONCONFIG_TITLE.'`,`'.TDE_RESERVATIONCONFIG_USER.'`,`'.TDE_RESERVATIONCONFIG_PASS.'`,`'.TDE_RESERVATIONCONFIG_LANG.'`,`'.TDE_RESERVATIONCONFIG_CPAGES.'`,`'.TDE_RESERVATIONCONFIG_MSG.'`,`'.TDE_RESERVATIONCALDELETED_FIELD.'`,calendar_mode) '.
+            ' VALUES("","'.$_GET["name"].'","","ENG","1","Please, select your reservation.","0","true");';
+
+    $wpdb->query($sql);   
+
+    $results = $wpdb->get_results('SELECT `'.TDE_RESERVATIONCONFIG_ID.'` FROM `'.DEX_RESERVATIONS_CONFIG_TABLE_NAME.'` ORDER BY `'.TDE_RESERVATIONCONFIG_ID.'` DESC LIMIT 0,1');        
+    $wpdb->query('UPDATE `'.DEX_RESERVATIONS_CONFIG_TABLE_NAME.'` SET `'.TDE_RESERVATIONCONFIG_TITLE.'`="cal'.$results[0]->id.'" WHERE `'.TDE_RESERVATIONCONFIG_ID.'`='.$results[0]->id);           
+    $message = "Item added";
+} 
+else if (isset($_GET['u']) && $_GET['u'] != '')
+{
+    $wpdb->query('UPDATE `'.DEX_RESERVATIONS_CONFIG_TABLE_NAME.'` SET conwer='.$_GET["owner"].',`'.TDE_RESERVATIONCALDELETED_FIELD.'`='.$_GET["public"].',`'.TDE_RESERVATIONCONFIG_USER.'`="'.$_GET["name"].'" WHERE `'.TDE_RESERVATIONCONFIG_ID.'`='.$_GET['u']);           
+    $message = "Item updated";        
+}
+else if (isset($_GET['d']) && $_GET['d'] != '')
+{
+    $wpdb->query('DELETE FROM `'.DEX_RESERVATIONS_CONFIG_TABLE_NAME.'` WHERE `'.TDE_RESERVATIONCONFIG_ID.'`='.$_GET['d']);       
+    $message = "Item deleted";
+}
+
+
+if ($message) echo "<div id='setting-error-settings_updated' class='updated settings-error'><p><strong>".$message."</strong></p></div>";
+
+?>
+<div class="wrap">
+<h2>CP Reservation Calendar</h2>
+
+
+
+
+<script type="text/javascript">
+ function cp_addItem()
+ {
+    var calname = document.getElementById("cp_itemname").value;
+    document.location = 'admin.php?page=dex_reservations&a=1&r='+Math.random()+'&name='+encodeURIComponent(calname);       
+ }
+ 
+ function cp_updateItem(id)
+ {
+    var calname = document.getElementById("calname_"+id).value;
+    var owner = document.getElementById("calowner_"+id).options[document.getElementById("calowner_"+id).options.selectedIndex].value;    
+    if (owner == '')
+        owner = 0;
+    var is_public = (document.getElementById("calpublic_"+id).checked?"0":"1");
+    document.location = 'admin.php?page=dex_reservations&u='+id+'&r='+Math.random()+'&public='+is_public+'&owner='+owner+'&name='+encodeURIComponent(calname);    
+ }
+ 
+ function cp_manageSettings(id)
+ {
+    document.location = 'admin.php?page=dex_reservations&cal='+id+'&r='+Math.random();
+ }
+ 
+ function cp_BookingsList(id)
+ {
+    document.location = 'admin.php?page=dex_reservations&cal='+id+'&list=1&r='+Math.random();
+ }
+ 
+ function cp_deleteItem(id)
+ {
+    if (confirm('Are you sure that you want to delete this item?'))
+    {        
+        document.location = 'admin.php?page=dex_reservations&d='+id+'&r='+Math.random();
+    }
+ }
+ 
+</script>
+
+
+<div id="normal-sortables" class="meta-box-sortables">
+
+
+ <div id="metabox_basic_settings" class="postbox" >
+  <h3 class='hndle' style="padding:5px;"><span>Calendar List / Items List</span></h3>
+  <div class="inside">
+  
+  
+  <table cellspacing="1"> 
+   <tr>
+    <th align="left">ID</th><th align="left">Calendar Name</th><th align="left">Owner</th><th align="left">Public</th><th align="left">&nbsp; &nbsp; Options</th>
+   </tr> 
+<?php  
+
+  $users = $wpdb->get_results( "SELECT user_login,ID FROM ".$wpdb->users." ORDER BY ID DESC" );                                                                     
+
+  $myrows = $wpdb->get_results( "SELECT * FROM ".DEX_RESERVATIONS_CONFIG_TABLE_NAME );                                                                     
+  foreach ($myrows as $item)   
+      if (cp_reservation_is_administrator() || ($current_user->ID == $item->conwer))
+      {
+?>
+   <tr> 
+    <td nowrap><?php echo $item->id; ?></td>
+    <td nowrap><input type="text" style="width:100px;" <?php if (!cp_reservation_is_administrator()) echo ' readonly '; ?>name="calname_<?php echo $item->id; ?>" id="calname_<?php echo $item->id; ?>" value="<?php echo esc_attr($item->uname); ?>" /></td>
+    
+    <?php if (cp_reservation_is_administrator()) { ?>
+    <td nowrap>
+      <select name="calowner_<?php echo $item->id; ?>" id="calowner_<?php echo $item->id; ?>">
+       <option value="0"<?php if (!$item->conwer) echo ' selected'; ?>></option>
+       <?php foreach ($users as $user) { 
+       ?>
+          <option value="<?php echo $user->ID; ?>"<?php if ($user->ID."" == $item->conwer) echo ' selected'; ?>><?php echo $user->user_login; ?></option>
+       <?php  } ?>
+      </select>
+    </td>    
+    <?php } else { ?>
+        <td nowrap>
+        <?php echo $current_user->user_login; ?>
+        </td>
+    <?php } ?>
+    
+    <td nowrap align="center">
+       <?php if (cp_reservation_is_administrator()) { ?> 
+         &nbsp; &nbsp; <input type="checkbox" name="calpublic_<?php echo $item->id; ?>" id="calpublic_<?php echo $item->id; ?>" value="1" <?php if (!$item->caldeleted) echo " checked "; ?> />
+       <?php } else { ?>  
+         <?php if (!$item->caldeleted) echo "Yes"; else echo "No"; ?> 
+       <?php } ?>   
+    </td>    
+    <td nowrap>&nbsp; &nbsp; 
+                             <?php if (cp_reservation_is_administrator()) { ?> 
+                               <input type="button" name="calupdate_<?php echo $item->id; ?>" value="Update" onclick="cp_updateItem(<?php echo $item->id; ?>);" /> &nbsp; 
+                             <?php } ?>    
+                             <input type="button" name="calmanage_<?php echo $item->id; ?>" value="Settings " onclick="cp_manageSettings(<?php echo $item->id; ?>);" /> &nbsp;                              
+                          
+    </td>
+   </tr>
+<?php  
+      } 
+?>   
+     
+  </table> 
+    
+<br /><br />
+<div style="border:1px solid #664444;background-color:#FDFDC8;width:90%;padding-left:10px;padding-right:10px;font-size:12px;">
+    <p><strong>Upgrade notice:</strong><br /><br />
+        This plugin is the <strong>free version of the CP Reservation Calendar</strong>. If you already have the pro version please use your personal download link to update it to the latest pro version.<br />
+        <br /><strong>Important note:</strong> There is a more advanced plugin named <strong><a href="http://wordpress.dwbooster.com/calendars/booking-calendar-contact-form?cpapp=up#download">Booking Calendar Contact Form</a></strong> developed based in this one that contains all the features present in this plugin plus a large additional set of features in both free and commercial versions, including (also in <strong>free version</strong>):
+    <br >
+    <br >&nbsp; - Season prices management
+    <br >&nbsp; - Supports bookings of a fixed length, example weekly bookings
+    <br >&nbsp; - Price structure can be defined for each number of days
+    <br >&nbsp; - Calendar configurable settings: date format, min/max dates, block dates, mark holidays, select working weekdays, calendar pages
+    <br >&nbsp; - Better jQuery based calendar
+    <br >&nbsp; - Language & translations support
+    <br >&nbsp; - Printable list of bookings
+    <br >&nbsp; - Many additional configuiration settings
+    <br >&nbsp; - ... and a lot more of features
+<br /><br />
+
+ To see the plugin details go to the <strong><a href="http://wordpress.dwbooster.com/calendars/booking-calendar-contact-form?cpapp=up#download">Booking Calendar Contact Form</a></strong> page.</p>
+</div>    
+    
+   
+  </div>    
+ </div> 
+ 
+  
+</div> 
+
+
+[<a href="http://wordpress.dwbooster.com/contact-us" target="_blank">Request Custom Modifications</a>] | [<a href="http://wordpress.dwbooster.com/calendars/cp-reservation-calendar" target="_blank">Help</a>]
+</form>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
