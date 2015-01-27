a62d09c89fe8e145e12b1c90bd5d65f49b2efbca
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-27 12:57:08 +0000

    fixed query used on multisite installations.
    git-svn-id: https://plugins.svn.wordpress.org/cp-reservation-calendar/tags/1.1.6@1076630 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dex_reservations.php
+++ dex_reservations.php
@@ -107,7 +107,7 @@ function dex_reservations_install($networkwide)  {
 		if ($networkwide) {
 	                $old_blog = $wpdb->blogid;
 			// Get all blog ids
-			$blogids = $wpdb->get_col($wpdb->prepare("SELECT blog_id FROM $wpdb->blogs"));
+			$blogids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
 			foreach ($blogids as $blog_id) {
 				switch_to_blog($blog_id);
 				_dex_reservations_install();
