be51cdf73033a1ee5b759b759b21945d5a5d419e
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-28 22:14:51 +0000

    Update to the function that generates the IPN url.
    git-svn-id: https://plugins.svn.wordpress.org/cp-reservation-calendar/trunk@1102125 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dex_reservations.php
+++ dex_reservations.php
@@ -758,6 +758,8 @@ function cp_reservation_get_FULL_site_url($admin = false)
     $pos = strpos($url, "://");    
     if ($pos === false)
         $url = 'http://'.$_SERVER["HTTP_HOST"].$url;
+    if (!empty($_SERVER['HTTPS']))     
+        $url = str_replace("http://","https://",$str);        
     return $url;
 }
 
