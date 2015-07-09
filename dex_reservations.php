199e4461ae3804d286aa80c0c2335569f458cbb5
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-09 09:50:26 +0000

    Fixed issue in HTTPs detection.
    git-svn-id: https://plugins.svn.wordpress.org/cp-reservation-calendar/tags/1.1.7@1195324 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dex_reservations.php
+++ dex_reservations.php
@@ -760,8 +760,8 @@ function cp_reservation_get_FULL_site_url($admin = false)
     $pos = strpos($url, "://");    
     if ($pos === false)
         $url = 'http://'.$_SERVER["HTTP_HOST"].$url;
-    if (!empty($_SERVER['HTTPS']))     
-        $url = str_replace("http://","https://",$url);        
+//    if (!empty($_SERVER['HTTPS']))     
+//        $url = str_replace("http://","https://",$url);        
     return $url;
 }
 
